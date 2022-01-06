e9e7253c30f32766b63128a0c0cb4c71c2328a0f
Umrzoq Toshkentov <u.toshkentov@SRT.local>
2022-01-06 11:43:50 +0500

    fix: webpack config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -11,9 +11,8 @@ const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const ImageminWebpack = require('image-minimizer-webpack-plugin');
 const paths = require('../config/paths');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { appBuild, appPublic, appHtml } = paths;
-
-// Source maps are resource heavy and can cause out of memory issue for large source files.
 
 module.exports = merge(common, {
   mode: 'production',
@@ -34,8 +33,6 @@ module.exports = merge(common, {
       ],
     }),
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: 'static/css/[name].[contenthash:8].css',
       chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
     }),
@@ -62,8 +59,6 @@ module.exports = merge(common, {
   output: {
     publicPath: '',
     pathinfo: true,
-    // There will be one main bundle, and one file per asynchronous chunk.
-    // In development, it does not produce real files.
     filename: 'static/js/[name].[chunkhash].js',
     // There are also additional JS chunk files if you use code splitting.
     chunkFilename: 'static/js/[name].[chunkhash].chunk.js',
@@ -90,45 +85,27 @@ module.exports = merge(common, {
       new TerserPlugin({
         terserOptions: {
           parse: {
-            // We want terser to parse ecma 8 code. However, we don't want it
-            // to apply any minification steps that turns valid ecma 5 code
-            // into invalid ecma 5 code. This is why the 'compress' and 'output'
-            // sections only apply transformations that are ecma 5 safe
-            // https://github.com/facebook/create-react-app/pull/4234
             ecma: 8,
           },
           compress: {
             ecma: 5,
             warnings: false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebook/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
             comparisons: false,
-            // Disabled because of an issue with Terser breaking valid code:
-            // https://github.com/facebook/create-react-app/issues/5250
-            // Pending further investigation:
-            // https://github.com/terser-js/terser/issues/120
             inline: 2,
           },
           mangle: {
             safari10: true,
           },
-          // Added for profiling in devtools
-          // keep_classnames: isEnvProductionProfile,
-          // keep_fnames: isEnvProductionProfile,
           output: {
             ecma: 5,
             comments: false,
-            // Turned on because emoji and regex is not minified properly using default
-            // https://github.com/facebook/create-react-app/issues/2488
-            // eslint-disable-next-line camelcase
             ascii_only: true,
           },
         },
       }),
       // This is only used in production mode
       new CssMinimizerPlugin(),
+      new UglifyJsPlugin(),
       new ImageminWebpack({
         severityError: 'warning', // Ignore errors on corrupted images
         minimizerOptions: {
