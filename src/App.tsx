2bf355c286f0ae131fdd905b0089b1ab13973b33
Umrzoq Toshkentov <u.toshkentov@SRT.local>
2022-01-06 09:32:19 +0500

    initialized application

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="main">
+      <h3>Application</h3>
+      <p>Running app</p>
+    </div>
+  );
+};
+
+export default App;
